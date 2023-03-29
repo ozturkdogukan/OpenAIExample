3ec6d7c3b91897939d64fd1a9103191a2115eb70
reatline <reatline@gmail.com>
2023-03-29 14:54:20 +0300

    Add project files.

---
+++ ConsoleApp5/OpenAI.cs
@@ -0,0 +1,42 @@
+﻿using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels.ResponseModels;
+using OpenAI.GPT3.ObjectModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAI
+{
+    public class OpenAI
+    {
+        readonly IOpenAIService _openAIService;
+
+        public OpenAI(IOpenAIService openAIService)
+        {
+            _openAIService = openAIService;
+        }
+
+        public async Task Task()
+        {
+            while (true)
+            {
+                Console.Write("::");
+                CompletionCreateResponse result = await _openAIService.Completions.CreateCompletion(new CompletionCreateRequest()
+                {
+                    Prompt = Console.ReadLine(),
+                    MaxTokens = 2048
+                }, Models.TextDavinciV3);
+
+                Console.WriteLine(result.Choices[0].Text);
+            }
+
+        }
+
+        
+
+        }
+    }
+
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenAI;
+using OpenAI.GPT3.Managers;
+using OpenAI.GPT3;
+
+Console.WriteLine("Hello, World!");
+
+var openAiService = new OpenAIService(new OpenAiOptions()
+{
+    ApiKey = "API-KEY"
+});
+
+OpenAI.OpenAI a = new OpenAI.OpenAI(openAiService);
+a.Task();
+Console.ReadKey();
